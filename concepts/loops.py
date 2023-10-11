91f8967a5d9ea5d36447510c109156656f14dcbe
sushant babu prasai <sushantprasaii7@gmail.com>
2023-10-11 18:43:49 +0545

    practices

---
+++ concepts/loops.py
@@ -0,0 +1,45 @@
+# let's dive deep into loops and continue loops evercise after all loops 
+
+# loop through string and add those string to list as individual items 
+
+# string = "python"
+# lists = []
+# for x in string: 
+#     lists.append(x)
+#     print(lists)
+
+
+# find all the odd number from 1 to n 
+# n = int(input("enter the number"))
+# for x in range(1,n+2,5):
+#     print(x)
+
+# print(range(5)) # range(0,5)
+
+# using brake statements
+# fruits = ["apple",'mango','strawberry','banana','cherry']
+# for i in fruits:
+#     print(i)
+#     if i == "mango":
+#         break
+
+# Note : the else will not be executed if the loop is stopped by a break statement
+
+# continue in python
+for i in range (8):
+    if i == 3:
+        continue 
+    print(i)
+
+
+
+
+# nested loops in python
+num1= [1,3,5,7]
+num2=[2,4,6,8]
+
+
+# pass statement does nothing but you can use pass to avoid getting error due to empty loops or conditionals
+
+
+
