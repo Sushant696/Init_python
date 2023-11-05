6c4e85b2343017fc565a6b5a6dc6d8d928ad076f
sushant babu prasai <sushantprasaii7@gmail.com>
2023-11-05 12:26:37 +0545

    hangman welcome

---
+++ mini_projects/hangman/hangman.py
@@ -0,0 +1,8 @@
+def welcome():
+    print('Welcome to the Hangman Game! Please enter your prefered game name: ')
+    name = input()
+    print(f"HI!{name} Let's begin the game")
+
+welcome()
+
+words : ['python','absurd','nightclub','quantum','computer','jukebox','strength','krugger','awareness','jiujitsu']
