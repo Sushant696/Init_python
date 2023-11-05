8299d57324b040f75b09f9253184050af020dfbc
sushant babu prasai <sushantprasaii7@gmail.com>
2023-11-05 13:53:41 +0545

    hangman init

--- mini_projects/hangman/hangman.py
+++ mini_projects/hangman/hangman.py
@@ -1,8 +1,58 @@
-def welcome():
-    print('Welcome to the Hangman Game! Please enter your prefered game name: ')
-    name = input()
-    print(f"HI!{name} Let's begin the game")
+import random
+from stages import logo
+print(logo)
 
-welcome()
+# def welcome():
+    # print('Welcome to the Hangman Game!,hangman_logo')
+#     name = input()
+#     print(f"HI!{name} Let's begin the game")
+# welcome()
 
-words : ['python','absurd','nightclub','quantum','computer','jukebox','strength','krugger','awareness','jiujitsu']
+word_list = [
+'python',
+'absurd',
+'nightclub',
+'quantum',
+'computer',
+"function",
+'jukebox',
+'strength',
+"conditional",
+'krugger', 
+'awareness',
+"debugging",
+'jiujitsu',
+"database",
+"optimization",
+"framework",
+"interface",
+"authentication",
+"version",
+"iteration",
+"repository",
+"variable",
+"authorization"
+
+,]
+
+# assign random word to the user
+chosen_word = random.choice(word_list)
+print('The number you have to guess is:',chosen_word)
+display = ['_']*len(chosen_word)
+print(display)
+
+
+# check if that word has that letter 
+# if the word has that letter then add that letter and display
+# use the max and min attempts concept  
+
+min_attempts = 0 
+max_attempts = 6
+
+while max_attempts >= min_attempts :
+    guess = input("guess the letter").lower()
+    if len(guess) != 1 or not guess.isalpha():
+        print("please enter a single letter:") 
+
+
+    min_attempts +=1
