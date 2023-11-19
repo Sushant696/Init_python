18f52d29d13bf806f104467a5e28be1b64d72faf
sushant babu prasai <sushantprasaii7@gmail.com>
2023-11-19 12:47:22 +0545

    first push

--- problems/function.py
+++ problems/function.py
@@ -8,3 +8,12 @@ def sum(numbers):
     return total
 print(sum((1,827,3,63,81,325,3)))
 # print(a)
+
+
+# create a function with variable length of arguments
+
+def hello (**params) :
+    c = a + b
+    return c 
+d = hello(1 , 4 , 9)
+print(d)
