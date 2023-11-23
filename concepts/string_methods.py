310db12ad14c339456ddfcdd01073ef6e4f9378c
sushant babu prasai <sushantprasaii7@gmail.com>
2023-11-23 16:29:00 +0545

    strings

--- concepts/string_methods.py
+++ concepts/string_methods.py
@@ -1,3 +1,10 @@
 # let's explore some string methods
 
 mystr = "sushant babu prasai"
+# a = mystr.expandtabs(8)
+# search for a value to get it's position 
+# a = mystr.find('a')
+# join at the end of the string 
+a = '_'.join(mystr)
+print(a)
+# this means join each items with the things specified
