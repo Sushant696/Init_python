08cab4560f4f8177f12af32858081bd38cdb85e4
sushant babu prasai <sushantprasaii7@gmail.com>
2023-12-07 21:50:14 +0545

    collage work

---
+++ softwarica/day3.py
@@ -0,0 +1,51 @@
+# Learning about if else , input method type conversion , implicit and explicit
+
+# getting input from user
+# a = input("hello world") # string
+
+# live saver eval function
+# user_input  = eval(input("Enter your age "))
+# print(type(user_input)) 1
+
+
+# """
+# Error handling
+
+# 1> run-time error 
+# those error that occurs while exectionof the code.
+#  For example  : 
+# infinite loop , accecssing the undefined variables
+
+
+# 2> compile erorr 
+# Those error that occurs 
+# # """
+
+# user_input = int(input('enter 1-7 to get the day'))
+
+# if(user_input == 1):
+#     print('sunday')
+# elif (user_input == 2):
+#     print("monday")
+# elif(user_input== 3):
+#     print('tuesday')
+# elif(user_input== 4):
+#     print('wednesday')
+# elif(user_input== 5):
+#     print('thursday')
+# elif(user_input== 6):
+#     print('friday')
+# elif(user_input== 7):
+#     print('saturday')
+# else :
+#     print("please input numbers from 1-7")
+
+
+
+userInput == int(input('enter you number'))
+
+if (userInput % 2 == 0):
+    print('The number you entered in even')
+else:
+    print('The number you entered is odd')
+
---
+++ softwarica/problems.py
@@ -0,0 +1,38 @@
+# userInput = int(input('enter you number'))
+
+
+# if (userInput % 2 == 0):
+#     print('The number you entered in even')
+# else:
+#     print('The number you entered is odd')
+
+
+# student_marks = eval(input('Enter your marks'))
+
+# if (student_marks >= 80 and student_marks <= 100):
+#     print("you got A")
+# elif (student_marks >= 60 and student_marks <= 80):
+#     print('you got b')
+# elif (student_marks >= 50 and student_marks <= 60):
+#     print('you got c')
+# elif (student_marks >= 45 and student_marks <= 50):
+#     print('you got d')
+# elif (student_marks >= 25 and student_marks <= 45):
+#     print('you got e')
+# elif (student_marks <= 25):
+#     print('you got f')
+# else:
+#     print('please enter valid marks')
+
+
+# m = eval (input('enter number '))
+
+# if (m > 0):
+#     n = 1
+#     print(f"m greater than 0 then n = {n}")
+# elif (m == 0):
+#     n  = 0
+#     print(f"m equalls to 0 than n  = {n}")
+# elif (m  < 0):
+#     n = -1
+#     print(f"m is less  n ={n}")
