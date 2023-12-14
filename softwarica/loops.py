af3269db7a100aeb272d8a88fbe5a8125e86bb34
sushant babu prasai <sushantprasaii7@gmail.com>
2023-12-14 11:53:07 +0545

    email and password validation

---
+++ softwarica/loops.py
@@ -0,0 +1,126 @@
+# # Exercise 6: Count the total number of digits in a number
+# # number = int(input('Enter a number'))
+# # count = 0
+
+# # while(number > 0):
+# #     number = number // 10 
+# #     count += 1 
+# #     print(number)
+# # print(f'the number of the digit is {count}')    
+
+# # Exercise 10: Use else block to display a message “Done” after successful execution of for loop
+
+# # for i in range(0,7,2):
+# #     print(i)
+# # else:
+# #     print('Done')
+
+# # # Exercise 11: Write a program to display all prime numbers within a range
+
+# # prime = []
+# # n = int(input("take the range of numbes"))
+# # flag = 0
+
+# # for i in range(2 , n + 1):
+# #     if i > 1 :
+
+# #         print('No prime number identified')
+# #         if(i % 2 != 0):
+# #             flag = 1
+# #     if (flag == 1):
+# #         print(f'{i} is prime number')
+# #         prime.append(i)
+# #     else:
+# #         print(f"{i} is not a prime number")
+
+# # print(prime)
+
+# # prime = []
+# # n = int(input("Enter the range of numbers: "))
+# # flag = 0
+
+# # for i in range(2, n + 1):
+# #     if i > 1:
+# #         flag = 1
+# #         for j in range(2, int(i/2) + 1):
+# #             if (i % j) == 0:
+# #                 flag = 0
+# #                 break
+# #         if flag == 1:
+# #             print(f'{i} is a prime number')
+# #             prime.append(i)
+# #         else:
+# #             print(f"{i} is not a prime number")
+
+# # print("Prime numbers within the range:", prime)
+
+
+
+# # for i in range(11):
+# #     result = 2 * i
+# #     print(f'2 * {i}={result}')
+# # Display the 2 times table
+
+# # for i in range(-1, -11):
+# #     result = 2 * i
+# #     print(f'2 * {i} = {result}')
+
+# # for i in range(10,0,-1):
+# #     result = 2 * i
+# #     print(f'2 * {i} = {result}')
+
+
+# # add items of list
+# # mylist = [1,2,3,4,5]
+# # add = 0 
+# # multiply = 1
+
+# # for i in mylist:
+# #     add += i 
+# #     multiply *= i
+# #     print(add , multiply)
+
+
+# # mylist = [1,2,3,4,5]
+
+# # odd =[]
+# # even=[]
+
+# # for i in mylist:
+# #     if(i%2==0):
+# #         print('even')
+# #         even.append(i)
+# #     else:
+# #         print('odd')
+# #         odd.append(i)
+# # print(odd , even)
+
+
+# mylist = ['a','b',1,2]
+
+# for i in mylist:
+#     print(f"{i} is of {type(i)}")
+
+
+tries = 3
+
+for i in range(4):
+    if tries != 0:
+        email = input("Input your email address: ")
+        password = input('Input your password: ')
+        length = len(password)
+
+        if length < 8 and email == '':
+            print(f'The password should be at least 8 characters long. Tries left: {tries}')
+            tries = tries - 1
+
+        elif not email.endswith('@gmail.com'):
+            tries = tries - 1
+            print(f'Please use a proper email format. Tries left: {tries}')
+        elif "#" not in password and '*' not in password:
+            tries = tries - 1
+            print(f"At least one special character needed. Tries left: {tries}")
+        else:
+            print('You have successfully logged in')
+            break
+
--- softwarica/validate.py
+++ softwarica/validate.py
@@ -7,29 +7,35 @@ emailaddress = input('Enter you emailaddress: ')
 len = len(password)
 
 
-
 # password validation
 def passwordFunc(password, len):
     if (len >= 8):
         if ('#' in password or "*" in password):
+            global validPassword
+            validPassword = 1
+            print(validPassword, 'hello')
             print("You password is valid and saved")
         else:
             print('please enter a valid password which contains a special character and have more than 8 characters')
     else:
         print("please enter a password that is more than 8 characters long")
+
+
 passwordFunc(password, len)
 
-#Email validation
+# Email validation
+print(validPassword, 'hello')
+
 
 def emailFunc(emailaddress):
     if (emailaddress != " "):
-        if(emailaddress.endswith("@gmail.com")):
+        if (emailaddress.endswith("@gmail.com") and validPassword):
             # return emailaddress
-            print("Your email is valid and is saved in your system")
+            print("Your password and email both are valid and saved")
         else:
             print('please enter a valid emailaddress with proper format')
     else:
         print("Email address cannot be empty")
 
-emailFunc(emailaddress)
 
+emailFunc(emailaddress)
