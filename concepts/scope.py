c939c3e93221d836c469ae004052574e5ccae1d5
sushant babu prasai <sushantprasaii7@gmail.com>
2024-01-05 11:34:57 +0545

    functions

--- concepts/scope.py
+++ concepts/scope.py
@@ -1,15 +1,15 @@
-# what is scope and concept of scope in python 
+# # what is scope and concept of scope in python 
 
-x = 4 
-print("global x",x)
+# x = 4 
+# print("global x",x)
 
-def  hello():
-    global x 
-    x = 2 
-    print('local  x',x)
+# def  hello():
+#     global x 
+#     x = 2 
+#     print('local  x',x)
 
-hello()
-print('global x' , x)
+# hello()
+# print('global x' , x)
 
 # the scoping is pretty simple in python there are just local and global scope,
 
@@ -17,3 +17,12 @@ print('global x' , x)
 # Global variables means variable that defined outside any particular function and is accessible to any function 
 
 # if you want to use an local  variable outside function or want to use the global variable inside function and manipulate it's value than you can use the global keyword
+
+
+sum = lambda x , y: x + y
+print(sum(2,3))
+
+# doesn't 
+
+
+
