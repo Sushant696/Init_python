35b4d7e5418072e66466240e306410888e1badaa
sushant babu prasai <sushantprasaii7@gmail.com>
2024-02-07 23:15:16 +0545

    questions

---
+++ code_snippets/questoins.py
@@ -0,0 +1,13 @@
+# # define the term isinstance
+# a = 'hello'
+# x = isinstance(a, str)
+# print(x)
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+car.popitem()
+
+print(car)
